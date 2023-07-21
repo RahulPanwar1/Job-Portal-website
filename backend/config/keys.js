3c392004a2ef6e8f66d913436ee2803b1edecfd0
Rahul Panwar <91124001+RahulPanwar1@users.noreply.github.com>
2023-07-21 13:55:06 +0530

    Update keys.js removed keys

--- backend/config/keys.js
+++ backend/config/keys.js
@@ -1,4 +1,4 @@
-dbPassword='mongodb+srv://suhani1230:Suhani1230@cluster0.qcod3.mongodb.net/test?retryWrites=true&w=majority'
+dbPassword='Enter your DB password'
 
 module.exports = {
   mongoURI: dbPassword
